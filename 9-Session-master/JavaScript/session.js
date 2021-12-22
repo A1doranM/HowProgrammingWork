dde095efb1068133fb2056ea453c10fd8f78c053
Maksym Medvediev <aldoran.ua@gmail.com>
2021-12-22 19:35:10 +0200

    l16

--- 9-Session-master/JavaScript/session.js
+++ 9-Session-master/JavaScript/session.js
@@ -20,6 +20,7 @@ const generateToken = () => { // Генерируем токен сессии
 };
 
 // Данный класс отвечает за получение куки и восстановление сессии, сессию мы храним на диске.
+// Наследуем его от Мар чтобы использовать класс как коллекцию элементов.
 class Session extends Map {
   constructor(token) {
     super();
@@ -30,22 +31,25 @@ class Session extends Map {
     if (client.session) return client.session;
     const token = generateToken();
     client.token = token;
-    const session = new Session(token);
+    const session = new Session(token); // Создаем новую сессию
     client.session = session;
-    client.setCookie("token", token);
-    storage.set(token, session);
+    client.setCookie("token", token); // устанавливаем куки
+    storage.set(token, session); // сохраняем сессию в хранилище, где ключем является токен
     return session;
   }
 
   static restore(client) {
-    const { cookie } = client;
+    const { cookie } = client; // забираем куки у клиента
     if (!cookie) return;
-    const sessionToken = cookie.token;
+    const sessionToken = cookie.token; // из куки берем токен сессии
     if (sessionToken) {
       return new Promise((resolve, reject) => {
         storage.get(sessionToken, (err, session) => {
           if (err) reject(new Error("No session"));
-          Object.setPrototypeOf(session, Session.prototype);
+          Object.setPrototypeOf(session, Session.prototype); // Сессия это восстановленный Мар, но он еще не связан с нашим классом сессии
+          // но из-за того что он был восстановлен из сериализованного формата который хранится в файле у него есть все
+          // аттрибуты из Мар но он пока не связан с сессией, а таким образом мы его связываем.
+          // Это происходит из-за того что изначально мы наследуемся от Мар.
           client.token = sessionToken;
           client.session = session;
           resolve(session);
--- 9-Session-master/JavaScript/storage.js
+++ 9-Session-master/JavaScript/storage.js
@@ -1,23 +1,28 @@
 "use strict";
 
+// Файл для управлением хранилища сесссий.
+
 const fs = require("fs");
 const path = require("path");
 const v8 = require("v8");
 
 const PATH = `${__dirname}/sessions`;
 
-const safePath = (fn) => (token, ...args) => {
+// Функция-обертка занимается резолвом токена в имя файла.
+// В возвращаемую функцию теперь вместо имени файла можно передавать токен.
+const safePath = (fn) => (token, ...args) => { // по контракту вначале идут все аргументы, а последним идет коллбэк
   const callback = args[args.length - 1];
-  if (typeof token !== "string") {
+  if (typeof token !== "string") { // Если токен не строка то завершаем программу.
     callback(new Error("Invalid session token"));
     return;
   }
-  const fileName = path.join(PATH, token);
-  if (!fileName.startsWith(PATH)) {
+  const fileName = path.join(PATH, token); // К пути доклеивам еще токен.
+  if (!fileName.startsWith(PATH)) { // Но если кто-то засунул в начало токена переход между папками (аля хацкер).
+    // Если путь не начинается на тот с по котрому хранятся все сессии то мы выходим из программы.
     callback(new Error("Invalid session token"));
     return;
   }
-  fn(fileName, ...args);
+  fn(fileName, ...args); // вызываем переданную нам функцию с именем файла и аргументами.
 };
 
 const readSession = safePath(fs.readFile);
