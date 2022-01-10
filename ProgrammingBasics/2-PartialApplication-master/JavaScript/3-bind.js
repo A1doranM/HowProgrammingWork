ba11dcdde6caadac4d8e7ecc8cb4b6a5fec566b3
A1doranM <vaselisk1997@gmail.com>
2022-01-10 16:44:51 +0200

    l14

---
+++ ProgrammingBasics/2-PartialApplication-master/JavaScript/3-bind.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const { log: ln } = Math;
+const log = (base, n) => ln(n) / ln(base);
+
+// Usage
+{
+  const lg = log.bind(null, 10);
+  const ln = log.bind(null, Math.E);
+
+  console.log("lg(5) = " + lg(5));
+  console.log("ln(5) = " + ln(5));
+}
