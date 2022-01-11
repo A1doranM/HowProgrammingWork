d2cace5392a95ff35fae3e6166bdaf616781de22
A1doranM <vaselisk1997@gmail.com>
2022-01-11 20:48:33 +0200

    l35

--- NodeJS/24-Logging-master/JavaScript/1-functional/logger.js
+++ NodeJS/24-Logging-master/JavaScript/1-functional/logger.js
@@ -5,11 +5,11 @@ const fs = require("fs");
 // file => serializer => app => module => kind => msg
 
 const logger = file => {
-  const { isTTY } = file;
+  const { isTTY } = file; // Если надо выводить в консоль.
   const stream = isTTY ? file : fs.createWriteStream(file);
-  return (serializer = logger.defaultSerializer) => app => module => kind => {
-    const color = isTTY ? (logger.colors[kind] || logger.colors.info) : "";
-    const normal = isTTY ? logger.colors.normal : "";
+  return (serializer = logger.defaultSerializer) => app => module => kind => { // Возвращаем каррированную функцию которая принимает в себя разные параметры.
+    const color = isTTY ? (logger.colors[kind] || logger.colors.info) : ""; // Переводим консоль в режим вывода определенным цветом.
+    const normal = isTTY ? logger.colors.normal : ""; // Возвращаем в нормальное состояние.
     return msg => {
       const date = new Date().toISOString();
       const record = { date, kind, app, module, msg };
@@ -19,6 +19,7 @@ const logger = file => {
   };
 };
 
+// Берем все значения из объекта и разделяем их табуляцией.
 logger.defaultSerializer = obj => Object.values(obj).join("\t");
 
 logger.colors = {
