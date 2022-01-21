f9802b027f7427e81e0d261d6f24631183dcbd83
A1doranM <vaselisk1997@gmail.com>
2022-01-21 11:28:00 +0200

    l21

--- ProgrammingBasics/8-Timers, Timeouts, EventEmitter/Timers-master/JavaScript/8-ref-unref.js
+++ ProgrammingBasics/8-Timers, Timeouts, EventEmitter/Timers-master/JavaScript/8-ref-unref.js
@@ -4,8 +4,9 @@ const timer = setTimeout(() => {}, 10000);
 
 console.dir(timer);
 
-if (process.argv[2] === "--unref") timer.unref();
-console.dir(timer);
+if (process.argv[2] === "--unref") timer.unref(); // Позволяет удалить ссылку на таймер, тоесть таймер запланируется
+console.dir(timer);                               // но если никто не будет удерживать приложение то оно заверщится
+                                                  // несмотря на незавершенные таймеры.
 
-if (process.argv[3] === "--ref") timer.ref();
-console.dir(timer);
+if (process.argv[3] === "--ref") timer.ref(); // Наоборот добавляет ссылку таймеру у которого вызвали unref делая из него
+console.dir(timer);                           // нормальный таймер
