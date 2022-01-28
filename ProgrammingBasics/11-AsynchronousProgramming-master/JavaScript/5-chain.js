257180e799e3ea994bdbf0c43f379234f3fc312c
A1doranM <vaselisk1997@gmail.com>
2022-01-28 16:24:07 +0200

    l26

--- ProgrammingBasics/11-AsynchronousProgramming-master/JavaScript/5-chain.js
+++ ProgrammingBasics/11-AsynchronousProgramming-master/JavaScript/5-chain.js
@@ -2,31 +2,43 @@
 
 // Use list and chaining syntax to build sequence
 
+// Пример с созданием цепи исполнения функций.
+
+// Строим двух связный список.
 const chain = (prev = null) => {
   console.log("Create element");
-  const cur = () => {
+  const cur = () => { // Создаем функцию которая возвращается из метода chain.
+                      // Функция начинает рекурсивно итерироваться в обратном порядке. И связывать односвязный список
+                      // в двух связный. Это делается для того, чтобы нам надо будет исполнить всю цепочку
+                      // мы могли начать ее исполнение в том порядке в котором нам передавались функции, если этого не
+                      // сделать то у всех функций есть ссылка только на предыдущий элемент и поэтому вызывать мы можем
+                      // их лишь с конца в начало.
     console.log("Reverse from " + (cur.fn ? cur.fn.name : "null"));
-    if (cur.prev) {
-      cur.prev.next = cur;
-      cur.prev();
+    if (cur.prev) { // Если есть предыдущий элемент
+      cur.prev.next = cur; // добавляем ему ссылку на следующий элемент
+      cur.prev(); // как только дошли в начало списка
     } else {
-      cur.forward();
+      cur.forward(); // вызываем форвард и начинается вызов функций в прямом порядке.
     }
   };
-  cur.prev = prev;
-  cur.fn = null;
-  cur.args = null;
-  cur.do = (fn, ...args) => {
-    cur.fn = fn;
-    cur.args = args;
-    return chain(cur);
+  cur.prev = prev; // К этой функции примешиваем prev который нам приходит.
+  cur.fn = null; // У функция которая будет навешана на элемент.
+  cur.args = null; // Аргументы этой функции.
+  cur.do = (fn, ...args) => { // Метод do добавляет новую функцию в список,
+                              // первый аргумент это функция, второй это аргументы.
+    cur.fn = fn; // Сохраняем переданную функцию.
+    cur.args = args; // Сохраняем аргументы.
+    return chain(cur); // Рекурсивно вызвали chain передав функцию cur и в итоге chain начнется с начала создав новый
+                      // элемент двух связного списка.
   };
-  cur.forward = () => {
+  cur.forward = () => { // Функция которая будет проходить по двухсвязному с писку начина
     console.log("Forward");
-    if (cur.fn) cur.fn(...cur.args, (err, data) => {
+    if (cur.fn) cur.fn(...cur.args, (err, data) => { // Если у текущего элемента есть функция. Вызовем ее с
+                                                    // текущими аргументами
       console.log("Callback from " + cur.fn.name);
       console.dir({ data });
-      if (!err && cur.next) cur.next.forward();
+      if (!err && cur.next) cur.next.forward(); // проверяем если не было ошибки и есть следующий элемент списка
+                                                // вызываем метод forward у него.
       else console.log("End at " + cur.fn.name);
     });
   };
