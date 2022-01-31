b515eaa6532a9bbb6ffcde35719ff7385d38bb60
A1doranM <vaselisk1997@gmail.com>
2022-01-31 16:08:35 +0200

    l29

---
+++ ProgrammingBasics/14-NonBlockingIteration-master/JavaScript/2-each-non-blocking.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const numbers = new Array(1000).fill(1);
+
+const each = (array, fn) => {
+  const last = array.length - 1;
+  const next = i => {
+    setTimeout(() => {
+      fn(array[i], i);
+      if (i !== last) next(++i);
+    }, 0);
+  };
+  next(0);
+};
+
+let k = 0;
+
+const timer = setInterval(() => {
+  console.log("next ", k++);
+}, 10);
+
+const begin = process.hrtime.bigint();
+
+each(numbers, (item, i) => {
+  console.log(i);
+  if (i === 999) {
+    clearInterval(timer);
+    const diff = (process.hrtime.bigint() - begin) / 1000000n;
+    console.log("Time(ms):", diff.toString());
+  }
+});
---
+++ ProgrammingBasics/14-NonBlockingIteration-master/JavaScript/7-async-array.js
@@ -0,0 +1,62 @@
+"use strict";
+
+const INTERVAL = 10;
+
+class AsyncArray extends Array {
+  interval(ms) {
+    this._interval = ms;
+    return this;
+  }
+
+  [Symbol.asyncIterator]() {
+    let time = Date.now();
+    let i = 0;
+    const interval = this._interval || INTERVAL;
+    return {
+      next: () => {
+        const now = Date.now();
+        const diff = now - time;
+        if (diff > interval) {
+          time = now;
+          return new Promise(resolve => {
+            setTimeout(() => {
+              resolve({
+                value: this[i],
+                done: i++ === this.length
+              });
+            }, 0);
+          });
+        }
+        return Promise.resolve({
+          value: this[i],
+          done: i++ === this.length
+        });
+      }
+    };
+  }
+}
+
+// Usage
+
+let k = 0;
+
+const timer = setInterval(() => {
+  console.log("next ", k++);
+}, 10);
+
+(async () => {
+  const numbers = new AsyncArray(1000)
+    .interval(10)
+    .fill(1);
+
+  const begin = process.hrtime.bigint();
+  let i = 0;
+  for await (const number of numbers) {
+    console.log(number, i++);
+  }
+  clearInterval(timer);
+
+  const diff = (process.hrtime.bigint() - begin) / 1000000n;
+  console.log("Time(ms):", diff.toString());
+  console.dir({ k });
+})();
