3757e0371b84bc0151bee277819279969d7157b2
A1doranM <vaselisk1997@gmail.com>
2022-02-01 14:36:08 +0200

    l48

--- NodeJS/30-Packages-main/JavaScript/6-conditional.js
+++ NodeJS/30-Packages-main/JavaScript/6-conditional.js
@@ -1,5 +1,8 @@
 "use strict";
 
+// Внутри package.json указанного пакета мы устанавливаем две точки входа,
+// одну для require другую для import.
+
 const p6 = require("Package6");
 console.log({ p6 });
 
--- NodeJS/30-Packages-main/JavaScript/7-ecma.mjs
+++ NodeJS/30-Packages-main/JavaScript/7-ecma.mjs
@@ -3,6 +3,10 @@ const m2 = await import("module");
 import m3 from "module";
 import m4 from "module";
 
+// У динамического импорта и у обычного, разные кеши.
+// При этом у require и у обычного import кеш общий.
+// При очистке кеша require он очистится и у обычного import но не очистится для динамического.
+
 const { createRequire } = m2;
 const require = createRequire(import.meta.url);
 
