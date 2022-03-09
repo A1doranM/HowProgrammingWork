4a115f3f00a5aed2d5432afec797aed7f8432069
A1doranM <aldoran.ua@gmail.com>
2022-03-09 16:06:53 +0200

    l66

--- ProgrammingBasics/38-ConcurrentQueue-master/JavaScript/1-channels.js
+++ ProgrammingBasics/38-ConcurrentQueue-master/JavaScript/1-channels.js
@@ -2,55 +2,57 @@
 
 class Queue {
   constructor(concurrency) {
-    this.concurrency = concurrency;
-    this.count = 0;
-    this.waiting = [];
-    this.onProcess = null;
+    this.concurrency = concurrency; // Количество каналов, или уровень конкурентности
+    this.count = 0; // сколько сейчас обрабатывается
+    this.waiting = []; // массив ожидающих исполнения
+    this.onProcess = null; // обработчики.
     this.onDone = null;
     this.onSuccess = null;
     this.onFailure = null;
     this.onDrain = null;
   }
 
-  static channels(concurrency) {
+  static channels(concurrency) { // Фабрика очередей.
     return new Queue(concurrency);
   }
 
-  add(task) {
-    const hasChannel = this.count < this.concurrency;
+  add(task) { // Добавляем таску в очередь.
+    const hasChannel = this.count < this.concurrency; // Проверяем есть ли свободный канал
     if (hasChannel) {
-      this.next(task);
+      this.next(task); // если есть то добавляем
       return;
     }
-    this.waiting.push(task);
+    this.waiting.push(task); // иначе ставим в очередь.
   }
 
-  next(task) {
-    this.count++;
-    this.onProcess(task, (err, result) => {
-      if (err) {
-        if (this.onFailure) this.onFailure(err);
-      } else if (this.onSuccess) {
-        this.onSuccess(result);
+  next(task) { // Начинает обработку таски.
+    this.count++; // Увеличили счетчик текущих тасок.
+    this.onProcess(task, (err, result) => { // Вызвали onProcess отдавая таск и коллбэк на выполнение.
+      if (err) { // Если вернулась ошибка
+        if (this.onFailure) this.onFailure(err); // и есть обработчик, то вызываем его.
+      } else if (this.onSuccess) { // Если нет ошибки
+        this.onSuccess(result); // вызываем обработчик успешного завершения задачи.
       }
-      if (this.onDone) this.onDone(err, result);
+      if (this.onDone) this.onDone(err, result); // Если есть обработчик на onDone то туда и ошибку и результат отдаем.
+                                                 // Это небольшое дублирование но так можно подписываться например только на результат, или ошибку, или и то и то.
       this.count--;
-      if (this.waiting.length > 0) {
-        const task = this.waiting.shift();
-        this.next(task);
-        return;
+      if (this.waiting.length > 0) { // Если кто-то ожидает
+        const task = this.waiting.shift(); // берем его
+        this.next(task); // опять вызываем next
+        return; // и выходим.
       }
-      if (this.count === 0 && this.onDrain) {
-        this.onDrain();
+      if (this.count === 0 && this.onDrain) { // Если никто не ждал и нет тасок
+        this.onDrain(); // начинаем ждать и вызывам onDrain.
       }
     });
   }
 
-  process(listener) {
+  process(listener) { // Устанавливает таску которую надо делать.
     this.onProcess = listener;
     return this;
   }
 
+  // Установщики обработчиков.
   done(listener) {
     this.onDone = listener;
     return this;
@@ -79,16 +81,16 @@ const job = (task, next) => {
   setTimeout(next, task.interval, null, task);
 };
 
-const queue = Queue.channels(3)
-  .process(job)
-  .done((err, res) => {
+const queue = Queue.channels(3) // Создаем трехканальную очередь
+  .process(job) // навешиваем процедуру которую будем выполнять
+  .done((err, res) => { // обработчик на завершение задачи в одном из каналов
     const { count } = queue;
     const waiting = queue.waiting.length;
     console.log(`Done: ${res.name}, count:${count}, waiting: ${waiting}`);
   })
   .success(res => console.log(`Success: ${res.name}`))
   .failure(err => console.log(`Failure: ${err}`))
-  .drain(() => console.log("Queue drain"));
+  .drain(() => console.log("Queue drain")); // обработчик когда очередь опустела.
 
 for (let i = 0; i < 10; i++) {
   queue.add({ name: `Task${i}`, interval: i * 1000 });
--- ProgrammingBasics/38-ConcurrentQueue-master/JavaScript/2-timeouts.js
+++ ProgrammingBasics/38-ConcurrentQueue-master/JavaScript/2-timeouts.js
@@ -1,5 +1,9 @@
 "use strict";
 
+// Очередь с максимальным инетрвалом ожидания и максимальным
+// интервалом обработки.
+// Все остальное почти так же.
+
 class Queue {
   constructor(concurrency) {
     this.concurrency = concurrency;
@@ -10,7 +14,7 @@ class Queue {
     this.onSuccess = null;
     this.onFailure = null;
     this.onDrain = null;
-    this.waitTimeout = Infinity;
+    this.waitTimeout = Infinity; // По умолчанию ждем бесконечно.
     this.processTimeout = Infinity;
   }
 
@@ -34,7 +38,7 @@ class Queue {
       this.next(task);
       return;
     }
-    this.waiting.push({ task, start: Date.now() });
+    this.waiting.push({ task, start: Date.now() }); // Теперь добавляем еще поле когда элемент пришел.
   }
 
   next(task) {
@@ -42,41 +46,41 @@ class Queue {
     let timer = null;
     let finished = false;
     const { processTimeout, onProcess } = this;
-    const finish = (err, res) => {
-      if (finished) return;
-      finished = true;
-      if (timer) clearTimeout(timer);
+    const finish = (err, res) => { // Функция которая вызывается на таймере, или на коллбэке.
+      if (finished) return; // Если флаг завершения задачи уже стоит значит сразу выходим.
+      finished = true; // Иначе ставим влаг в true.
+      if (timer) clearTimeout(timer); // Очищаем таймер на всякий случай.
       this.count--;
-      this.finish(err, res);
-      if (this.waiting.length > 0) this.takeNext();
+      this.finish(err, res); // Вызываем финиш, но не этот а у самого класса.
+      if (this.waiting.length > 0) this.takeNext(); // Если кто-то еще ждет, вызываем следующую задачу.
     };
-    if (processTimeout !== Infinity) {
-      timer = setTimeout(() => {
-        timer = null;
+    if (processTimeout !== Infinity) { // Если таймаут задан
+      timer = setTimeout(() => { // Дополнительно ставим таймаут
+        timer = null; // При срабатываении обнуляем таймер
         const err = new Error("Process timed out");
-        finish(err, task);
+        finish(err, task); // в финиш отдаем ошибку и задачу.
       }, processTimeout);
     }
-    onProcess(task, finish);
+    onProcess(task, finish); // Запускаем выполнение задачи.
   }
 
-  takeNext() {
-    const { waiting, waitTimeout } = this;
-    const { task, start } = waiting.shift();
-    if (waitTimeout !== Infinity) {
-      const delay = Date.now() - start;
-      if (delay > waitTimeout) {
+  takeNext() { // Берет следующую задачу.
+    const { waiting, waitTimeout } = this; // Берем всех ожидающих и таймаут.
+    const { task, start } = waiting.shift(); // Берем следующую задачу.
+    if (waitTimeout !== Infinity) { // Если есть таймаут ожидания
+      const delay = Date.now() - start; // смотрим не протухли ли они.
+      if (delay > waitTimeout) { // Если протухли.
         const err = new Error("Waiting timed out");
-        this.finish(err, task);
-        if (waiting.length > 0) this.takeNext();
+        this.finish(err, task); // Завершаем с ошибкой.
+        if (waiting.length > 0) this.takeNext(); // И если кто-то еще ждет то вызываем следующую задачу.
         return;
       }
     }
-    this.next(task);
+    this.next(task); // Если нету таймаута просто вызываем следующую задачу.
     return;
   }
 
-  finish(err, res) {
+  finish(err, res) { // Проверяет события и вызывает их.
     const { onFailure, onSuccess, onDone, onDrain } = this;
     if (err) {
       if (onFailure) onFailure(err, res);
@@ -120,8 +124,8 @@ const job = (task, next) => {
 };
 
 const queue = Queue.channels(3)
-  .wait(4000)
-  .timeout(5000)
+  .wait(4000) // Сколько ожидать.
+  .timeout(5000) // Сколько максимум выполнять.
   .process(job)
   .success(task => console.log(`Success: ${task.name}`))
   .failure((err, task) => console.log(`Failure: ${err} ${task.name}`))
