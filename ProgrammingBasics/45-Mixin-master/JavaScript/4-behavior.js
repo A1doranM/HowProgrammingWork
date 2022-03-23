7287a712870067dca03f600ce14557d6cb73a12f
A1doranM <aldoran.ua@gmail.com>
2022-03-23 14:06:22 +0200

    l85

---
+++ ProgrammingBasics/45-Mixin-master/JavaScript/4-behavior.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const scalable = image => {
+  image.scale = () => console.log("Image scaled");
+};
+
+const lazy = image => {
+  const scale = image.scale;
+  image.scale = () => setImmediate(() => scale());
+};
+
+const image = {};
+
+console.log("Mixin scalable() adds method: scale");
+scalable(image);
+console.log("Before scale");
+image.scale();
+console.log("After scale\n");
+
+console.log("Mixin lazy() adds lazy behavior");
+lazy(image);
+console.log("Before scale");
+image.scale();
+console.log("After scale");
