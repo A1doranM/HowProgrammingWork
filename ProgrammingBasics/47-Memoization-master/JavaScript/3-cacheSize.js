168bdb099e1c6720ec39c5407e023a0dd071c4f8
A1doranM <aldoran.ua@gmail.com>
2022-03-30 17:10:18 +0300

    l87

---
+++ ProgrammingBasics/47-Memoization-master/JavaScript/3-cacheSize.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const argKey = x => x.toString() + ":" + typeof x;
+const generateKey = args => args.map(argKey).join("|");
+
+const memoize = (fn, length) => {
+  const cache = new Map();
+  return (...args) => {
+    const key = generateKey(args);
+    console.log(`${fn.name}(${key}) call`);
+    if (cache.has(key)) return cache.get(key);
+    console.log(`max(${key}) calculate`);
+    const res = fn(...args);
+    if (cache.size >= length) {
+      const firstKey = cache.keys().next().value;
+      console.log("Delete key:", firstKey);
+      cache.delete(firstKey);
+    }
+    cache.set(key, res);
+    return res;
+  };
+};
+
+// Usage
+
+const max = (a, b) => (a > b ? a : b);
+const mMax = memoize(max, 3);
+
+mMax(10, 8);
+mMax(10, 8);
+mMax(1, 15);
+mMax(12, 3);
+mMax(15, 2);
+mMax(1, 15);
+mMax(10, 8);
+mMax(0, 0);
+mMax(0, 0);
