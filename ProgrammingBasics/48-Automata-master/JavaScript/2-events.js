1ec5b2888a4ad1ebf76ba50918fac9912b9417ed
A1doranM <vaselisk1997@gmail.com>
2022-04-04 15:54:47 +0300

    l91

---
+++ ProgrammingBasics/48-Automata-master/JavaScript/2-events.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const TIME_STEP = 500;
+
+const STATE_INIT = 0;
+const STATE_WORK = 1;
+const STATE_FIN = 2;
+const STATE_EXIT = 3;
+
+let state = STATE_INIT;
+
+process.on("SIGINT", () => state = STATE_FIN);
+
+const timer = setInterval(step, TIME_STEP);
+
+function step() {
+  switch (state) {
+  case STATE_INIT:
+    console.log("initialization");
+    state = STATE_WORK;
+    break;
+  case STATE_WORK:
+    process.stdout.write(".");
+    break;
+  case STATE_FIN:
+    console.log("\nfinalization");
+    state = STATE_EXIT;
+    break;
+  case STATE_EXIT:
+    console.log("exit");
+    clearInterval(timer);
+  }
+}
--- ProgrammingBasics/48-PrototypeChain-master/README.md
+++
@@ -1,3 +0,0 @@
-## Прототипное программирование и прототипное наследование
-
-[![Прототипное программирование и прототипное наследование](https://img.youtube.com/vi/SzaXTW2qcJE/0.jpg)](https://www.youtube.com/watch?v=SzaXTW2qcJE)
