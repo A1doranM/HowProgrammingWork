e67df48ec275af8474e3e332df5778803a6027dc
A1doranM <vaselisk1997@gmail.com>
2022-04-05 14:46:11 +0300

    l91

--- ProgrammingBasics/48-Automata-master/JavaScript/8-parser.js
+++ ProgrammingBasics/48-Automata-master/JavaScript/8-parser.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// Парсер и стейт машина разнесены по разным абстракциям.
+// Парсер и стейт машина разнесены по разным абстракциям. Найболее правильный вариант.
 
 class StateMachine {
     constructor(meta) {
@@ -18,10 +18,19 @@ class StateMachine {
     feed(char) {
         const {state, meta, buffer} = this;
         const node = meta[state];
+        console.log("\n ----------------------", "\nCurrent node, state, meta, buffer, char.");
+        console.log("Node: ", node,
+            "\nState: ", state,
+            "\nMeta: ", meta,
+            "\nBuffer: ", buffer,
+            "\nChar: ", char,
+            "\n"
+        );
         for (const key of Object.keys(node)) {
             if (key.includes(char) || key === "") {
                 const next = node[key];
-                console.log(`${char}: ${state} --> ${next}`);
+                console.log(`Key, next: ${key} --> ${next}`, "\n ----------------------");
+                console.log(`${char}: ${state} --> ${next}`, "\n ----------------------");
                 if (state !== next) {
                     this.state = next;
                     this.buffer = key === "" ? char : "";
