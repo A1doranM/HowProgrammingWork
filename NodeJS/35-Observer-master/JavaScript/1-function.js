7405bbb809c577cb4d78de56eb4ee5e7466737af
A1doranM <vaselisk1997@gmail.com>
2022-04-16 13:31:13 +0300

    Async Programming l-22

---
+++ NodeJS/35-Observer-master/JavaScript/1-function.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const randomChar = () => String
+  .fromCharCode(Math.floor((Math.random() * 25) + 97));
+
+const subscribe = observer => {
+  const observable = { observer };
+  setInterval(() => {
+    const char = randomChar();
+    observer(char);
+  }, 200);
+  return observable;
+};
+
+// Usage
+
+let count = 0;
+
+const observer = char => {
+  process.stdout.write(char);
+  count++;
+  if (count > 50) {
+    process.stdout.write("\n");
+    process.exit(0);
+  }
+};
+
+const observable = subscribe(observer);
+
+console.dir({ observer, observable });
---
+++ NodeJS/35-Observer-master/JavaScript/2-class.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const randomChar = () => String
+  .fromCharCode(Math.floor((Math.random() * 25) + 97));
+
+class Observable {
+  constructor() {
+    this.observer = null;
+    setInterval(() => {
+      if (!this.observer) return;
+      const char = randomChar();
+      this.observer(char);
+    }, 200);
+  }
+
+  subscribe(observer) {
+    this.observer = observer;
+    return this;
+  }
+}
+
+// Usage
+
+let count = 0;
+
+const observer = char => {
+  process.stdout.write(char);
+  count++;
+  if (count > 50) {
+    process.stdout.write("\n");
+    process.exit(0);
+  }
+};
+
+const observable = new Observable().subscribe(observer);
+
+console.dir({ observer, observable });
