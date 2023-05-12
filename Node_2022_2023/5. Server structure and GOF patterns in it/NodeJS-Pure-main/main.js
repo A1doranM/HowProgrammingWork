23d71159402edf197d2bdec13f1813821f37c041
A1doranM <vaselisk1997@gmail.com>
2023-05-12 14:35:40 +0300

    Node 2022-23 GOF patterns and server structure

---
+++ Node_2022_2023/5. Server structure and GOF patterns in it/NodeJS-Pure-main/main.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const path = require("node:path");
+
+const console = require("./lib/logger.js");
+const common = require("./lib/common.js");
+
+const { loadDir } = require("./src/loader.js");
+const { Server } = require("./src/server.js");
+
+const appPath = path.join(process.cwd(), "../NodeJS-Application");
+const api = Object.freeze({});
+const sandbox = { console, common, api, db: null };
+
+(async () => {
+  const configPath = path.join(appPath, "./config");
+  const configData = await loadDir(configPath, sandbox);
+  const config = Object.fromEntries(configData);
+
+  const db = require("./lib/db.js")(config.db);
+  sandbox.db = Object.freeze(db);
+
+  const apiPath = path.join(appPath, "./api");
+  const routing = await loadDir(apiPath, sandbox, true);
+
+  const application = { path: appPath, sandbox, console, config, routing };
+  application.server = new Server(application);
+})();
---
+++ Node_2022_2023/5. Server structure and GOF patterns in it/NodeJS-Pure-main/src/loader.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const fsp = require("node:fs").promises;
+const vm = require("node:vm");
+const path = require("node:path");
+
+const OPTIONS = {
+  timeout: 5000,
+  displayErrors: false,
+};
+
+const load = async (filePath, sandbox, contextualize = false) => {
+  const src = await fsp.readFile(filePath, "utf8");
+  const opening = contextualize ? "(context) => " : "";
+  const code = `"use strict";\n${opening}${src}`;
+  const script = new vm.Script(code, { ...OPTIONS, lineOffset: -1 });
+  const context = vm.createContext(Object.freeze({ ...sandbox }));
+  return script.runInContext(context, OPTIONS);
+};
+
+const loadDir = async (dir, sandbox, contextualize = false) => {
+  const files = await fsp.readdir(dir, { withFileTypes: true });
+  const container = new Map();
+  for (const file of files) {
+    const { name } = file;
+    if (file.isFile() && !name.endsWith(".js")) continue;
+    const location = path.join(dir, name);
+    const prefix = path.basename(name, ".js");
+    const loader = file.isFile() ? load : loadDir;
+    const exp = await loader(location, sandbox, contextualize);
+    if (exp.constructor.name === "Map") {
+      for (const [key, value] of exp) {
+        container.set(prefix + "." + key, value);
+      }
+    } else {
+      container.set(prefix, exp);
+    }
+  }
+  return container;
+};
+
+module.exports = { load, loadDir };
