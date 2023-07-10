65e7b1f49d7ae8438f2d02eb96a79b35872741f7
A1doranM <vaselisk1997@gmail.com>
2023-07-10 12:00:26 +0300

    Updated Bluetooth chapter

--- NodeJS/9-Session-master/JavaScript/storage.js
+++ NodeJS/9-Session-master/JavaScript/storage.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// Файл для управлением хранилища сесссий.
+// Файл для управлением хранилища сессий.
 
 const fs = require("fs");
 const path = require("path");
@@ -10,20 +10,25 @@ const PATH = `${__dirname}/sessions`;
 
 // Функция-обертка занимается резолвом токена в имя файла.
 // В возвращаемую функцию теперь вместо имени файла можно передавать токен.
-const safePath = (fn) => (token, ...args) => { // по контракту вначале идут все аргументы, а последним идет коллбэк.
-  const callback = args[args.length - 1];
-  if (typeof token !== "string") { // Если токен не строка то завершаем программу.
-    callback(new Error("Invalid session token"));
-    return;
-  }
-  const fileName = path.join(PATH, token); // К пути доклеивам еще токен.
-  if (!fileName.startsWith(PATH)) { // Проверяем не засунул ли кто-то в начало токена переход между папками "../" (аля хацкер),
-    // если путь не начинается на тот с по котрому хранятся все сессии то мы выходим из программы.
-    callback(new Error("Invalid session token"));
-    return;
-  }
-  fn(fileName, ...args); // вызываем переданную нам функцию с именем файла и аргументами.
-};
+const safePath =
+  (fn) =>
+  (token, ...args) => {
+    // по контракту вначале идут все аргументы, а последним идет коллбэк.
+    const callback = args[args.length - 1];
+    if (typeof token !== "string") {
+      // Если токен не строка то завершаем программу.
+      callback(new Error("Invalid session token"));
+      return;
+    }
+    const fileName = path.join(PATH, token); // К пути доклеивам еще токен.
+    if (!fileName.startsWith(PATH)) {
+      // Проверяем не засунул ли кто-то в начало токена переход между папками "../" (аля хацкер),
+      // если путь не начинается на тот с по котрому хранятся все сессии то мы выходим из программы.
+      callback(new Error("Invalid session token"));
+      return;
+    }
+    fn(fileName, ...args); // вызываем переданную нам функцию с именем файла и аргументами.
+  };
 
 const readSession = safePath(fs.readFile);
 const writeSession = safePath(fs.writeFile);
@@ -36,7 +41,8 @@ class Storage extends Map {
       callback(null, value);
       return;
     }
-    readSession(key, (err, data) => { // то читаем сессию с диска.
+    readSession(key, (err, data) => {
+      // то читаем сессию с диска.
       if (err) {
         callback(err);
         return;
@@ -52,7 +58,8 @@ class Storage extends Map {
     const value = super.get(key); // Берем наш токен.
     if (value) {
       const data = v8.serialize(value); // Сериализируем его
-      writeSession(key, data, () => { // и записываем в фаил.
+      writeSession(key, data, () => {
+        // и записываем в фаил.
         console.log(`Session saved: ${key}`);
       });
     }
