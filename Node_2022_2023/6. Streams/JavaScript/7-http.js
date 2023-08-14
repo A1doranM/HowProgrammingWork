4f07cae1f94315e8bd16fd54a4fecac15c4abece
A1doranM <vaselisk1997@gmail.com>
2023-08-14 19:05:23 +0300

    Updated Classless Addressing chapter

---
+++ Node_2022_2023/6. Streams/JavaScript/7-http.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const fs = require("node:fs");
+const zlib = require("node:zlib");
+const http = require("node:http");
+
+const readable = fs.createReadStream("index.html");
+const gzip = zlib.createGzip();
+
+const buffers = [];
+let buffer = null;
+
+gzip.on("data", (buffer) => {
+  buffers.push(buffer);
+});
+
+gzip.on("end", () => {
+  buffer = Buffer.concat(buffers);
+});
+
+readable.pipe(gzip);
+
+const server = http.createServer((request, response) => {
+  console.log(request.url);
+  response.writeHead(200, { "Content-Encoding": "gzip" });
+  response.end(buffer);
+});
+
+server.listen(8000);
