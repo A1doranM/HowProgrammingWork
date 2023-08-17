79eebcf98c52d6b3711eb2fd780efd7e31ee4935
A1doranM <vaselisk1997@gmail.com>
2023-08-17 13:50:26 +0300

    Streams: Readable, Writable, backpressure

---
+++ Node_2022_2023/7. Node-WASM/WAT/2-esm.mjs
@@ -0,0 +1,21 @@
+import fsp from 'node:fs/promises';
+
+const context = {
+  example: {
+    addCallback: (res) => {
+      console.log({ add: [10, 2], res, expected: 12 });
+    },
+    subCallback: (res) => {
+      console.log({ sub: [20, 5], res, expected: 15 });
+    },
+  },
+};
+
+const wasm = await fsp.readFile('./example.wasm');
+const example = await WebAssembly.instantiate(wasm, context);
+
+const res = example.instance.exports.sum(3, 7);
+console.log({ sum: [3, 7], res, expected: 10 });
+
+example.instance.exports.add(10, 2);
+example.instance.exports.sub(20, 5);
