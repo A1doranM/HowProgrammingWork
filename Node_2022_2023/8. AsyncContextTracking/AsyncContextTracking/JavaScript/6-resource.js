d3600d5035a7271ae4de1af18f998f7c3a89ea68
A1doranM <vaselisk1997@gmail.com>
2023-10-05 18:47:25 +0300

    Added Async context tracking chapter

---
+++ Node_2022_2023/8. AsyncContextTracking/AsyncContextTracking/JavaScript/6-resource.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const { AsyncResource, executionAsyncId } = require("node:async_hooks");
+
+const resource1 = new AsyncResource("name");
+const resource2 = new AsyncResource("name");
+
+console.log({ resource1 });
+console.log({ resource2 });
+
+const fn = () => {
+  const id = executionAsyncId();
+  console.log({ id });
+};
+
+fn();
+resource1.runInAsyncScope(fn);
+resource2.runInAsyncScope(fn);
+fn();
