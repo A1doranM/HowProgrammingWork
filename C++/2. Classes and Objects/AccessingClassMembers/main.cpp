982802dafdf5ec5af7f99e531320f2bdd35b4ac7
A1doranM <vaselisk1997@gmail.com>
2023-10-23 20:07:30 +0300

    Added C++ chapter

--- C++/2. Classes and Objects/AccessingClassMembers/main.cpp
+++ C++/2. Classes and Objects/AccessingClassMembers/main.cpp
@@ -11,7 +11,7 @@ public:
     string name;
     int health;
     int xp;
-    
+
     // methods
     void talk(string text_to_say) { cout << name << " says " << text_to_say << endl;  }
     bool is_dead();
@@ -22,36 +22,34 @@ public:
     // attributes
     string name;
     double balance;
-    
+
     // methods
     bool deposit(double bal) {balance += bal; cout << "In deposit" << endl; }
     bool withdraw(double bal) {balance -= bal; cout << "In withdraw" << endl;  }
-    
+
 };
 
 int main() {
     Account frank_account;
     frank_account.name = "Frank's account";
     frank_account.balance = 5000.0;
-    
+
     frank_account.deposit(1000.0);
     frank_account.withdraw(500.0);
-    
-    
+
+
     Player frank;
     frank.name = "Frank";
     frank.health = 100;
     frank.xp = 12;
     frank.talk("Hi there");
-    
+
     Player *enemy = new Player;
     (*enemy).name = "Enemy";
     (*enemy).health = 100;
     enemy->xp = 15;
-    
-    enemy->talk("I will destroy you!");
-    
 
+    enemy->talk("I will destroy you!");
 
     return 0;
 }
