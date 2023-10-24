232fedc3503fefdbad7ff344cbaaccd9145c688c
A1doranM <vaselisk1997@gmail.com>
2023-10-24 13:58:27 +0300

    Added C++ chapter

--- C++/2. Classes and Objects/DelegatingConstructors/main.cpp
+++ C++/2. Classes and Objects/DelegatingConstructors/main.cpp
@@ -18,27 +18,33 @@ public:
     Player(std::string name_val, int health_val, int xp_val);
 };
 
-Player::Player() 
+// Delegating constructor is about situation when we have a lot of similar code
+// in constructors so we can use another constructor inside constructor.
+
+// Here we use most specific constructor to create object without args
+Player::Player()
     : Player {"None",0,0} {
         cout << "No-args constructor" << endl;
 }
 
-Player::Player(std::string name_val) 
+// Here we pass just one argument.
+Player::Player(std::string name_val)
    : Player {name_val,0, 0}  {
            cout << "One-arg constructor" << endl;
 }
-  
-Player::Player(std::string name_val, int health_val, int xp_val) 
+
+// Our most specific constructor which set values for all fields.
+Player::Player(std::string name_val, int health_val, int xp_val)
     : name{name_val}, health{health_val}, xp{xp_val} {
             cout << "Three-args constructor" << endl;
 }
 
 int main() {
-    
+
     Player empty;
     Player frank {"Frank"};
     Player villain {"Villain", 100, 55};
-    
+
     return 0;
 }
 
