d2f20cc9807809777517fc3d6f1b9576067c4de7
A1doranM <vaselisk1997@gmail.com>
2023-10-24 13:36:57 +0300

    Added C++ chapter

--- C++/2. Classes and Objects/ImplementingMethods2/Account.h
+++ C++/2. Classes and Objects/ImplementingMethods2/Account.h
@@ -1,5 +1,10 @@
-#ifndef _ACCOUNT_H_
-#define _ACCOUNT_H_
+// Check to prevent multiple initializations of one class
+// without it if we write "#include "Account.h" multiple times in different
+// files we will receive error;
+// This is the common syntax for all header files in C++.
+
+#ifndef _ACCOUNT_H_ // if not define
+#define _ACCOUNT_H_ // define
 #include <string>
 
 class Account {
@@ -7,17 +12,17 @@ private:
     // attributes
     std::string name;
     double balance;
-    
+
 public:
     // methods
     // declared inline
     void set_balance(double bal) { balance = bal; }
     double get_balance() { return balance; }
-    
+
     // methods will be declared outside the class declaration
     void set_name(std::string n);
     std::string get_name();
-    
+
     bool deposit(double amount);
     bool withdraw(double amount);
 };
