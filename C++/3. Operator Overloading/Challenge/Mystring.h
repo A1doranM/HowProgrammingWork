fbf518e8b4ffcde70633178fc624ceb519aec5d5
A1doranM <vaselisk1997@gmail.com>
2023-10-30 15:08:41 +0200

    Added C++ chapter

--- C++/3. Operator Overloading/Challenge/Mystring.h
+++ C++/3. Operator Overloading/Challenge/Mystring.h
@@ -9,18 +9,18 @@ class Mystring
 private:
     char *str;      // pointer to a char[] that holds a C-style string
 public:
-    Mystring();                                                         // No-args constructor
-    Mystring(const char *s);                                     // Overloaded constructor
-    Mystring(const Mystring &source);                    // Copy constructor
-    Mystring( Mystring &&source);                         // Move constructor
-    ~Mystring();                                                     // Destructor
-    
-    Mystring &operator=(const Mystring &rhs);     // Copy assignment
-    Mystring &operator=(Mystring &&rhs);           // Move assignment
-    
+    Mystring();                        // No-args constructor
+    Mystring(const char *s);           // Overloaded constructor
+    Mystring(const Mystring &source);  // Copy constructor
+    Mystring( Mystring &&source);      // Move constructor
+    ~Mystring();                       // Destructor
+
+    Mystring &operator=(const Mystring &rhs);// Copy assignment
+    Mystring &operator=(Mystring &&rhs);     // Move assignment
+
     void display() const;
-    
-    int get_length() const;                                      // getters
+
+    int get_length() const;                 // getters
     const char *get_str() const;
 };
 
