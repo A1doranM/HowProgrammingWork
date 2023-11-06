4156746ed6c9edd9521a866bca3b6637e8be607d
A1doranM <vaselisk1997@gmail.com>
2023-11-06 15:05:47 +0200

    Added C++ chapter

--- C++/7. Exception Handling/CatchCPPExceptions/main.cpp
+++ C++/7. Exception Handling/CatchCPPExceptions/main.cpp
@@ -1,11 +1,12 @@
 // Section 18
 // catching std::exception s
+
 #include <iostream>
 #include <string>
 #include <vector>
 
 int main() {
-    
+
     try {
         for (int i=0; i< 100; i++)
             std::string *s = new std::string[9999999];
@@ -13,7 +14,7 @@ int main() {
     catch (const std::bad_alloc &ex) {
         std::cerr << ex.what() << std::endl;
     }
-    
+
     std::vector<int> vec {1,2,3,4,5};
     try {
         std::cout << vec.at(10);
@@ -21,7 +22,7 @@ int main() {
     catch (const std::out_of_range &ex) {
         std::cerr << ex.what() << std::endl;
     }
-    
+
     std::cout << "Bye" << std::endl;
     return 0;
 }
