9293d8217866ea61bed9d6c1d0e63d872f57106f
A1doranM <vaselisk1997@gmail.com>
2023-11-10 16:14:23 +0200

    Added C++ chapter

--- C++/9. Standard Template Library (STL)/2. FunctionTemplates/main.cpp
+++ C++/9. Standard Template Library (STL)/2. FunctionTemplates/main.cpp
@@ -39,28 +39,28 @@ int main() {
     std::cout << x << ", " << y << std::endl;
     my_swap(x, y);
     std::cout << x << ", " << y << std::endl;
-    
+
     Person p1 {"Curly", 15};
     Person p2 {"Moe", 30};
-    
+
     Person p3 = min(p1, p2);
     std::cout << p3.name << " is younger" << std::endl;
-      
+
     func(p1, p2);
-    
-    std::cout << min<int>(2,3) << std::endl;        // 2
-    std::cout << min(2,3) << std::endl;                 // 2
-    std::cout << min('A', 'B') << std::endl;            // A
-    std::cout << min(12.5, 9.2) << std::endl;        // 9.2
+
+    std::cout << min<int>(2,3) << std::endl;    // 2
+    std::cout << min(2,3) << std::endl;         // 2
+    std::cout << min('A', 'B') << std::endl;    // A
+    std::cout << min(12.5, 9.2) << std::endl;   // 9.2
     std::cout << min(5+2*2, 7+40) << std::endl; // 9
-    
+
     func<int,int>(10,20);
     func(10,20);
     func<char, double>('A', 12.4);
     func('A', 12.4);
     func(1000, "Testing");
     func(2000, std::string{"Frank"});
-    
+
 
     return 0;
 }
