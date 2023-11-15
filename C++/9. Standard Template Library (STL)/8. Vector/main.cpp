bb39f7abeb8c633329cb06913ceb38bb3e0cbf2a
A1doranM <vaselisk1997@gmail.com>
2023-11-15 17:01:28 +0200

    Added C++ chapter

--- C++/9. Standard Template Library (STL)/8. Vector/main.cpp
+++ C++/9. Standard Template Library (STL)/8. Vector/main.cpp
@@ -11,7 +11,7 @@ class Person {
     int age;
 public:
     Person() = default;
-    Person(std::string name, int age) 
+    Person(std::string name, int age)
         : name{name}, age{age}  {}
     bool operator<(const Person &rhs) const {
         return this->age < rhs.age;
@@ -49,10 +49,10 @@ void test1() {
 
     std::vector<int> vec {1,2,3,4,5};
     display(vec);
-    
+
     vec = {2,4,5,6};
     display2(vec);
-    
+
     std::vector<int> vec1 (10, 100);    // ten 100s in the vector
     display(vec1);
 }
@@ -65,51 +65,51 @@ void test2() {
     std::cout << "\nvec size: " << vec.size() << std::endl;
     std::cout << "vec max size: " << vec.max_size() << std::endl;
     std::cout << "vec capacity: " << vec.capacity() << std::endl;
-    
+
     vec.push_back(6);
     display(vec);
     std::cout << "\nvec size: " << vec.size() << std::endl;
     std::cout << "vec max size: " << vec.max_size() << std::endl;
     std::cout << "vec capacity: " << vec.capacity() << std::endl;
-    
-    vec.shrink_to_fit();    // C++11
+
+    vec.shrink_to_fit(); // Reduces capacity to size of vector    // C++11
     display(vec);
     std::cout << "\nvec size: " << vec.size() << std::endl;
     std::cout << "vec max size: " << vec.max_size() << std::endl;
     std::cout << "vec capacity: " << vec.capacity() << std::endl;
-    
+
     vec.reserve(100);
     display(vec);
     std::cout << "\nvec size: " << vec.size() << std::endl;
     std::cout << "vec max size: " << vec.max_size() << std::endl;
     std::cout << "vec capacity: " << vec.capacity() << std::endl;
-    
+
 }
 void test3() {
     std::cout << "\nTest3 =========================" << std::endl;
 
     std::vector<int> vec {1,2,3,4,5};
     display(vec);
-    
+
     vec[0] = 100;
     vec.at(1) = 200;
-    
+
     display(vec);
 }
 
 void test4() {
     std::cout << "\nTest4 =========================" << std::endl;
     std::vector<Person> stooges;
-    
+
     Person p1 {"Larry", 18};
     display(stooges);
-    
+
     stooges.push_back(p1);
     display(stooges);
-    
+
     stooges.push_back(Person{"Moe", 25});
     display(stooges);
-    
+
     stooges.emplace_back("Curly", 30);      // Use emplace_back!!!
     display(stooges);
 }
@@ -122,11 +122,11 @@ void test5() {
         {"Moe", 25},
         {"Curly", 30}
     };
-    
+
     display(stooges);
     std::cout << "\nFront: " << stooges.front() << std::endl;
     std::cout << "Back: " << stooges.back() << std::endl;
-    
+
     stooges.pop_back();
     display(stooges);
 }
@@ -135,21 +135,21 @@ void test6() {
     std::cout << "\nTest6 =========================" << std::endl;
     std::vector<int> vec {1,2,3,4,5};
     display(vec);
-    
+
     vec.clear();    // remove all elements
     display(vec);
-    
+
     vec = {1,2,3,4,5,6,7,8,9,10};
     display(vec);
     vec.erase(vec.begin(), vec.begin() + 2);
     display(vec);
-    
+
     vec = {1,2,3,4,5,6,7,8,9,10};
     // erase all even numbers
     auto it = vec.begin();
     while (it != vec.end()) {
         if (*it %2 == 0)
-            vec.erase(it);  
+            vec.erase(it);
         else
             it++;   // only increment if not erased!
     }
@@ -158,10 +158,10 @@ void test6() {
 
 void test7() {
     std::cout << "\nTest7 =========================" << std::endl;
-    
+
     std::vector<int> vec1 {1,2,3,4,5};
     std::vector<int> vec2 {10,20,30,40,50};
-    
+
     display(vec1);
     display(vec2);
     std::cout << std::endl;
@@ -174,26 +174,26 @@ void test7() {
 void test8() {
     std::cout << "\nTest8  =========================" << std::endl;
 
-    std::vector<int> vec1 {1,21,3,40,12};    
-    
+    std::vector<int> vec1 {1,21,3,40,12};
+
     display(vec1);
     std::sort(vec1.begin(), vec1.end());
     display(vec1);
 }
 
 void test9() {
-    // std::back_inserter contructs a back-insert iterator that inserts new elements 
+    // std::back_inserter contructs a back-insert iterator that inserts new elements
     // at the end of the container it applied to. It's a special output iterator
     // It's awesome!!!!   and  very efficient
     // There is also a front_inserter we can use with deques and lists
     // Copy one list to another using an iterator and back_inserter
-    
+
     std::cout << "\nTest9  =========================" << std::endl;
-    
-    std::vector<int> vec1 {1,2,3,4,5};   
+
+    std::vector<int> vec1 {1,2,3,4,5};
     std::vector<int> vec2 {10,20};
-    
-    display(vec1); 
+
+    display(vec1);
     display(vec2);
     std::cout << std::endl;
 
@@ -202,40 +202,40 @@ void test9() {
     display(vec2);
     std::cout << std::endl;
 
-    
+
     // Copy_if the element is even
-    
+
     vec1 = {1,2,3,4,5,6,7,8,9,10};
     vec2 = {10,20};
-    
+
     display(vec1);
     display(vec2);
     std::cout << std::endl;
-    
+
     std::copy_if(vec1.begin(), vec1.end(), std::back_inserter(vec2),
             [](int x) { return x%2 == 0; });
     display(vec1);
     display(vec2);
-    
-    
+
+
 }
 
 void test10() {
     std::cout << "\nTest10  =========================" << std::endl;
     // transform over 2 ranges
-    
-    std::vector<int> vec1 {1,2,3,4,5};   
+
+    std::vector<int> vec1 {1,2,3,4,5};
     std::vector<int> vec2 {10,20,30,40,50};
     std::vector<int> vec3;
-    
-    
+
+
     // 1*10, 2*20, 3*30, 4*40, 5*50 and store the results in vec3
     std::transform(vec1.begin(), vec1.end(), vec2.begin(),
         std::back_inserter(vec3),
         [](int x, int y) { return x * y;});
-        
+
     display(vec3);
-    
+
 }
 
 
@@ -246,12 +246,12 @@ void test11() {
     std::cout << "\nTest11  =========================" << std::endl;
     std::vector<int> vec1 {1,2,3,4,5,6,7,8,9,10};
     std::vector<int> vec2 {100,200,300,400};
-    
+
     display(vec1);
     display(vec2);
     std::cout << std::endl;
 
-    
+
     auto it = std::find(vec1.begin(), vec1.end(), 5);
     if (it != vec1.end()) {
         std::cout << "inserting..." << std::endl;
@@ -262,7 +262,7 @@ void test11() {
     display(vec1);
 }
 
-int main()  {    
+int main()  {
 
     test1();
     test2();
@@ -275,6 +275,6 @@ int main()  {
     test9();
     test10();
     test11();
-    
+
     return 0;
 }
