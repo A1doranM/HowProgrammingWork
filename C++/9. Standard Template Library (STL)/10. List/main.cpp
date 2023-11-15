47ce540c12afdcb1cf33a178d2b92fe5c19e18a0
A1doranM <vaselisk1997@gmail.com>
2023-11-15 17:18:09 +0200

    Added C++ chapter

--- C++/9. Standard Template Library (STL)/10. List/main.cpp
+++ C++/9. Standard Template Library (STL)/10. List/main.cpp
@@ -5,13 +5,26 @@
 #include <algorithm>
 #include <iterator> // for std::advance
 
+// Difference between forward_list and std::list
+
+/*
+    - uni-directional (singly linked)
+    - less overhead than a std::list
+    - direct element access is not provided
+    - rapid insertion and deletion of elements anywhere in the container
+    - reverse iterators not available. Iterators invalidate when corresponding
+      element  is deleted
+
+*/
+
+
 class Person {
     friend std::ostream &operator<<(std::ostream &os, const Person &p);
     std::string name;
     int age;
 public:
     Person() : name{"Unknown"}, age{0} {}
-    Person(std::string name, int age) 
+    Person(std::string name, int age)
         : name{name}, age{age}  {}
     bool operator<(const Person &rhs) const {
         return this->age < rhs.age;
@@ -41,16 +54,16 @@ void test1() {
 
     std::list<int> l1 {1,2,3,4,5};
     display(l1);
-    
+
     std::list<std::string> l2;
     l2.push_back("Back");
     l2.push_front("Front");
     display(l2);
-    
+
     std::list<int> l3;
     l3 = {1,2,3,4,5,6,7,8,9,10};
     display(l3);
-    
+
     std::list<int> l4 (10, 100);
     display(l4);
 }
@@ -61,10 +74,10 @@ void test2() {
     std::list<int> l {1,2,3,4,5,6,7,8,9,10};
     display(l);
     std::cout << "Size: " << l.size() << std::endl;
-    
+
     std::cout << "Front : " << l.front() << std::endl;
     std::cout << "Back  : " << l.back() << std::endl;
-    
+
     l.clear();
     display(l);
     std::cout << "Size: " << l.size() << std::endl;
@@ -75,17 +88,17 @@ void test3() {
 
     std::list<int> l {1,2,3,4,5,6,7,8,9,10};
     display(l);
-    
+
     l.resize(5);
     display(l);
-    
+
     l.resize(10);
     display(l);
-    
+
     std::list<Person> persons;
     persons.resize(5);             // uses the Person default constructor
     display(persons);
-    
+
 }
 void test4() {
     std::cout << "\nTest4 =========================" << std::endl;
@@ -97,17 +110,17 @@ void test4() {
         l.insert(it, 100);
     }
     display(l);
-    
+
     std::list<int> l2 {1000,2000,3000};
     l.insert(it, l2.begin(), l2.end());
     display(l);
-    
+
     std::advance(it, -4);       // point to the 100
     std::cout << *it << std::endl;
-    
+
     l.erase(it);                    // remove the 100 - iterator becomes invalid
     display(l);
-    
+
 }
 
 void test5() {
@@ -118,7 +131,7 @@ void test5() {
         {"Moe", 25},
         {"Curly", 17}
     };
-    
+
     display(stooges);
     std::string name;
     int age{};
@@ -126,26 +139,26 @@ void test5() {
     getline(std::cin, name);
     std::cout << "Enter their age: ";
     std::cin >> age;
-    
+
     stooges.emplace_back(name, age);
     display(stooges);
-    
+
     // Insert Frank before Moe
     auto it = std::find(stooges.begin(), stooges.end(), Person{"Moe", 25});
     if (it != stooges.end())
         stooges.emplace(it, "Frank", 18);
-    display(stooges);    
+    display(stooges);
 }
 
 void test6() {
     std::cout << "\nTest6 =========================" << std::endl;
-    
+
     std::list<Person>  stooges {
         {"Larry", 18},
         {"Moe", 25},
         {"Curly", 17}
     };
-    
+
     display(stooges);
     stooges.sort();
     display(stooges);
@@ -153,7 +166,7 @@ void test6() {
 
 
 int main() {
-    
+
     test1();
     test2();
     test3();
