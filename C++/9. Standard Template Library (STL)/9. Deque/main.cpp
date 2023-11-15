c5d120d19766cbd724655d554d1e2c65121e90db
A1doranM <vaselisk1997@gmail.com>
2023-11-15 17:08:30 +0200

    Added C++ chapter

--- C++/9. Standard Template Library (STL)/9. Deque/main.cpp
+++ C++/9. Standard Template Library (STL)/9. Deque/main.cpp
@@ -1,5 +1,5 @@
 // Section 20
-// Deque
+// Deque - double ended queue
 #include <iostream>
 #include <deque>
 #include <vector>
@@ -19,13 +19,13 @@ void test1() {
 
     std::deque<int> d {1,2,3,4,5};
     display(d);
-    
+
     d = {2,4,5,6};
     display(d);
-    
+
     std::deque<int> d1 (10, 100);    // ten 100s in the deque
     display(d1);
-    
+
     d[0]  = 100;
     d.at(1) = 200;
     display(d);
@@ -37,32 +37,32 @@ void test2() {
 
     std::deque<int> d {0,0,0};
     display(d);
-    
+
     d.push_back(10);
     d.push_back(20);
     display(d);
-    
+
     d.push_front(100);
     d.push_front(200);
     display(d);
-    
+
     std::cout << "Front: " << d.front() << std::endl;
     std::cout << "Back : " << d.back() << std::endl;
     std::cout << "Size  : " << d.size() << std::endl;
-    
+
     d.pop_back();
     d.pop_front();
     display(d);
 }
 
 void test3() {
-    // insert all even numbers into the back of a deque and all 
+    // insert all even numbers into the back of a deque and all
     // odd numbers into the front
     std::cout << "\nTest3 =========================" << std::endl;
-    
+
     std::vector<int> vec {1,2,3,4,5,6,7,8,9,10};
     std::deque<int> d;
-    
+
     for (const auto &elem: vec) {
         if (elem % 2 == 0)
             d.push_back(elem);
@@ -78,14 +78,14 @@ void test4() {
 
     std::vector<int> vec {1,2,3,4,5,6,7,8,9,10};
     std::deque<int> d;
-    
+
     for (const auto &elem: vec) {
         d.push_front(elem);
-    }    
+    }
     display(d);
-    
+
     d.clear();
-    
+
      for (const auto &elem: vec) {
         d.push_back(elem);
     }
@@ -98,12 +98,12 @@ void test5() {
 
     std::vector<int> vec {1,2,3,4,5,6,7,8,9,10};
     std::deque<int> d;
-    
+
     std::copy(vec.begin(), vec.end(), std::front_inserter(d));
     display(d);
-    
+
     d.clear();
-    
+
     std::copy(vec.begin(), vec.end(), std::back_inserter(d));
     display(d);
 }
