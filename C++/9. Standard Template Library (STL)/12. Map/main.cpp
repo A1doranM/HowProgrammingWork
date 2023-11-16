c168416d67f7523d0024b950c2e7ee056c129886
A1doranM <vaselisk1997@gmail.com>
2023-11-16 12:07:03 +0200

    Added C++ chapter

--- C++/9. Standard Template Library (STL)/12. Map/main.cpp
+++ C++/9. Standard Template Library (STL)/12. Map/main.cpp
@@ -34,56 +34,56 @@ void test1() {
         {"Moe", 1},
         {"Curly", 2}
     };
-    display(m);      
-    
+    display(m);
+
     m.insert( std::pair<std::string, int>("Anna", 10));
     display(m);
-    
+
     m.insert(std::make_pair("Joe", 5));
     display(m);
-    
+
     m["Frank"] = 18;
     display(m);
-    
-    m["Frank"] += 10;               
+
+    m["Frank"] += 10;
     display(m);
-    
+
     m.erase("Frank");
     display(m);
-    
+
     std::cout << "Count for Joe: " << m.count("Joe") << std::endl;
     std::cout << "Count for Frank: " << m.count("Frank") << std::endl;
-    
+
     auto it = m.find("Larry");
     if (it != m.end())
-        std::cout << "Found: " << it->first << " : " << it->second << std::endl;    
-        
+        std::cout << "Found: " << it->first << " : " << it->second << std::endl;
+
     m.clear();
-    
+
     display(m);
 
 }
 
 void test2() {
     std::cout << "\nTest2 =========================" << std::endl;
-    
+
     std::map<std::string, std::set<int>> grades  {
         {"Larry", {100, 90} },
         {"Moe",  {94} },
         {"Curly", {80, 90, 100} }
     };
-    
+
     display(grades);
-    
-    grades["Larry"].insert(95);                 // insert 95 into the set of grades for "Larry"
-    
+
+    grades["Larry"].insert(95);     // insert 95 into the set of grades for "Larry"
+
     display(grades);
-    
-    auto it = grades.find("Moe");
-    if (it != grades.end())                         // found "Moe"
+
+    auto it = grades.find("Moe");     // returns iterator which can iterate through the key and value pair
+    if (it != grades.end())           // found "Moe"
     {
-            it->second.insert(1000);            // insert 1000 into Moe's set of grades
-    }                                                         // it->first is the key, it->second is the value (set)
+            it->second.insert(1000);  // insert 1000 into Moe's set of grades
+    }                                 // it->first is the key, it->second is the value (set)
     display(grades);
 }
 
