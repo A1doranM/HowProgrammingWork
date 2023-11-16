fd716e066131a42dbe91566833860c0bdc546f4e
A1doranM <vaselisk1997@gmail.com>
2023-11-16 15:24:37 +0200

    Added C++ chapter

--- C++/9. Standard Template Library (STL)/13. Stack/main.cpp
+++ C++/9. Standard Template Library (STL)/13. Stack/main.cpp
@@ -5,10 +5,20 @@
 #include <vector>
 #include <list>
 
-// This function displays a stack of 
+// This function displays a stack of
 // by repeatedly topping and popping the stack
 // Note theat the stack is passed in by value so we
 // don't affect the passed in stack.
+
+/*
+
+    - LIFO data structure
+    - Implemented as an adapter over other STL container. Can be implemented as a
+      vector, list, or deque
+    - All operations occur on one end of the stack (top)
+    - No iterators supported
+
+*/
 
 template <typename T>
 void display(std::stack<T> s) {
@@ -26,27 +36,27 @@ int main() {
     std::stack<int, std::vector<int>> s1;
     std::stack<int, std::list<int>> s2;
     std::stack<int, std::deque<int>> s3;
-    
-    for (int i: {1,2,3,4,5}) 
+
+    for (int i: {1,2,3,4,5})
         s.push(i);
     display(s);
 
     s.push(100);
     display(s);
-    
+
     s.pop();
     s.pop();
     display(s);
-    
+
     while (!s.empty())
         s.pop();
     display(s);
-    
+
     std::cout << "Size: " << s.size() << std::endl;
-    
+
     s.push(10);
     display(s);
-    
+
     s.top() = 100;
     display(s);
     return 0;
