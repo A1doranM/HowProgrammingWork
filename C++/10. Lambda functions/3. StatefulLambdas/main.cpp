705e18615ce2cbe810cc2f00890630eed7b111e6
A1doranM <vaselisk1997@gmail.com>
2023-11-17 13:19:16 +0200

    Added C++ chapter

--- C++/10. Lambda functions/3. StatefulLambdas/main.cpp
+++ C++/10. Lambda functions/3. StatefulLambdas/main.cpp
@@ -4,16 +4,30 @@
 
 int global_x {1000};
 
+/*
+
+    Default capture modes for lambdas:
+        - [=] - Default capture by value
+        - [&] - Default capture by reference
+        - [this] - Default capture this object by reference
+
+    Examples:
+        - [=, &x] // Default capture by value but capture X by reference
+        - [&]     // Default capture by reference
+        - [&, y]  // Default capture by reference by capture Y by value
+
+*/
+
 // Capture by value
-// The global variable global_x cannot be captured because it is not 
-// within the reaching scope of the lambda. It can however still be accessed 
+// The global variable global_x cannot be captured because it is not
+// within the reaching scope of the lambda. It can however still be accessed
 // from within the lambda using normal name lookup rules.
 void test1()
 {
 	std::cout << "\n---Test1 --------------------------" << std::endl;
-	
+
 	int local_x {100};
-	
+
 	auto l = [local_x] () {
 		std::cout << local_x << std::endl;
 		std::cout << global_x << std::endl;
@@ -23,22 +37,22 @@ void test1()
 
 // Capture by value - mutable
 // The captured variable x can only be modified within the lambda by
-// using the mutable keyword. Note that this modifiability only pertains to 
+// using the mutable keyword. Note that this modifiability only pertains to
 // the captured variable within the lambda body and not the actual variable.
 void test2()
 {
 	std::cout << "\n---Test2 --------------------------" << std::endl;
-	
+
 	int x {100};
-	
+
 	auto l = [x] () mutable {
 		x += 100;
 		std::cout << x << std::endl;
 	};
-    
+
 	l();
 	std::cout << x << std::endl;
-    
+
     l();
     std::cout << x << std::endl;
 }
@@ -49,14 +63,14 @@ void test2()
 void test3()
 {
 	std::cout << "\n---Test3 --------------------------" << std::endl;
-	
+
 	int x {100};
-	
+
 	auto l = [&x] () mutable {
 		x += 100;
 		std::cout << x << std::endl;
 	};
-    
+
 	l();
 	std::cout << x << std::endl;
 }
@@ -67,12 +81,12 @@ void test3()
 void test4()
 {
 	std::cout << "\n---Test4 --------------------------" << std::endl;
-	
+
 	int x {100};
 	int y {200};
 	int z {300};  // Note that z is not captured in the lambda since it is not used.
                       // This should produce an 'unused variable' warning when compiling.
-	
+
 	auto l = [=] () mutable {
 		x += 100;
 		y += 100;
@@ -80,7 +94,7 @@ void test4()
 		std::cout << y << std::endl;
 	};
 	l();
-	
+
 	std::cout << "\n";
 	std::cout << x << std::endl;
 	std::cout << y << std::endl;
@@ -92,11 +106,11 @@ void test4()
 void test5()
 {
 	std::cout << "\n---Test5 --------------------------" << std::endl;
-	
+
 	int x {100};
 	int y {200};
 	int z {300};
-	
+
 	auto l = [&] () {
 		x += 100;
 		y += 100;
@@ -106,7 +120,7 @@ void test5()
 		std::cout << z << std::endl;
 	};
 	l();
-	
+
 	std::cout << "\n";
 	std::cout << x << std::endl;
 	std::cout << y << std::endl;
@@ -117,11 +131,11 @@ void test5()
 void test6()
 {
 	std::cout << "\n---Test6 --------------------------" << std::endl;
-	
+
 	int x {100};
 	int y {200};
 	int z {300};
-	
+
 	auto l = [=, &y] () mutable {
 		x += 100;
 		y += 100;
@@ -131,7 +145,7 @@ void test6()
 		std::cout << z << std::endl;
 	};
 	l();
-	
+
 	std::cout << "\n";
 	std::cout << x << std::endl;
 	std::cout << y << std::endl;
@@ -143,11 +157,11 @@ void test6()
 void test7()
 {
 	std::cout << "\n---Test7 --------------------------" << std::endl;
-	
+
 	int x {100};
 	int y {200};
 	int z {300};
-	
+
 	auto l = [&, x, z] () mutable {
 		x += 100;
 		y += 100;
@@ -157,7 +171,7 @@ void test7()
 		std::cout << z << std::endl;
 	};
 	l();
-	
+
 	std::cout << "\n";
 	std::cout << x << std::endl;
 	std::cout << y << std::endl;
@@ -179,11 +193,11 @@ public:
     void set_name(std::string name) {this->name = name; };
     int get_age() const {return age; }
     void set_age(int age) {this->age = age; }
-	
+
 	auto change_person1() {return [this] (std::string new_name, int new_age) {name = new_name; age = new_age;};}
 	auto change_person2() {return [=] (std::string new_name, int new_age) {name = new_name; age = new_age;};}
 	auto change_person3() {return [&] (std::string new_name, int new_age) {name = new_name; age = new_age;};}
-    
+
 };
 
 std::ostream &operator<<(std::ostream &os, const Person &rhs) {
@@ -201,19 +215,19 @@ void test8()
 
 	Person person("Larry", 18);
 	std::cout << person << std::endl;
-	
+
 	// Default capture [this]
     // This is the preferred way
 	auto change_person1 = person.change_person1();
 	change_person1("Moe", 30);
 	std::cout << person << std::endl;
-	
-	// Default capture [=] 
+
+	// Default capture [=]
     // This has been deprecated in C++20
 	auto change_person2 = person.change_person2();
 	change_person2("Curly", 25);
 	std::cout << person << std::endl;
-	
+
 	// Default capture [&]
 	auto change_person3 = person.change_person3();
 	change_person3("Frank", 34);
@@ -226,7 +240,7 @@ private:
 	int y;
 public:
 	Lambda(int y) : y{y} {};
-	
+
 	void operator() (int x) const {
 		std::cout << x + y << std::endl;
 	};
@@ -236,12 +250,12 @@ public:
 void test9()
 {
 	std::cout << "\n---Test9 --------------------------" << std::endl;
-	
+
 	int y {100};
-	
+
 	Lambda lambda1(y);
 	auto lambda2 = [y] (int x) {std::cout << x + y << std::endl;};
-	
+
 	lambda1(200);
 	lambda2(200);
 }
@@ -262,16 +276,16 @@ public:
     int get_max_people() const {
         return max_people;
     }
-    
+
     // This method returns a vector of Person objects in
-    // people whose age > max_age AND it limits the number of 
+    // people whose age > max_age AND it limits the number of
     // persons returned to be <= the member variable max_people.
     // Note that the lambda in this example captures this, by reference, and by value
     std::vector<Person> get_people(int max_age) {
         std::vector<Person> result;
         int count{0};
-        std::copy_if(people.begin(), 
-                           people.end(), 
+        std::copy_if(people.begin(),
+                           people.end(),
                            std::back_inserter(result),
                             [this, &count, max_age](const Person &p){ return p.get_age() > max_age &&  ++count<=max_people;});
         return result;
@@ -288,23 +302,23 @@ void test10() {
     friends.add("Moe", 35);
     friends.add("Frank", 28);
     friends.add("James", 65);
-    
+
     auto result = friends.get_people(17);
-    
+
     std::cout << std::endl;
     for (const auto &p: result)
         std::cout << p << std::endl;
-        
+
     friends.set_max_people(3);
-    
+
     result = friends.get_people(17);
-    
+
     std::cout << std::endl;
     for (const auto &p: result)
         std::cout << p << std::endl;
-        
+
     result = friends.get_people(50);
-    
+
     std::cout << std::endl;
     for (const auto &p: result)
         std::cout << p << std::endl;
@@ -322,7 +336,7 @@ int main()
 	test8();
 	test9();
     test10();
-	
+
 	std::cout << "\n";
 	return 0;
 }
