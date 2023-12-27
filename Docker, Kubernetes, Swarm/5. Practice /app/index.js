eb1b13900796ff2f8774c01ca1b6150bc0546910
A1doranM <vaselisk1997@gmail.com>
2023-12-27 11:28:11 +0200

    Added Microservices Patterns chapter

---
+++ Docker, Kubernetes, Swarm/5. Practice /app/index.js
@@ -0,0 +1,21 @@
+const http2 = require('node:http2');
+const fs = require('node:fs');
+
+const server = http2.createSecureServer({
+  key: fs.readFileSync('localhost-privkey.pem'),
+  cert: fs.readFileSync('localhost-cert.pem'),
+});
+server.on('error', (err) => console.error(err));
+
+server.on('stream', (stream, headers) => {
+  // stream is a Duplex
+  stream.respond({
+    'content-type': 'text/html; charset=utf-8',
+    ':status': 200,
+  });
+  stream.end('<h1>Hello World</h1>');
+});
+
+console.log('Server listening port 8443');
+
+server.listen(8443);
