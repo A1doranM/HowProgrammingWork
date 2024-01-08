da34e19eb0fda9d456bdb069c86206cb436f3510
A1doranM <vaselisk1997@gmail.com>
2024-01-08 13:33:25 +0200

    Fixes

--- Node_2022_2023/6. Streams/JavaScript/c-cork.js
+++ Node_2022_2023/6. Streams/JavaScript/c-cork.js
@@ -61,11 +61,11 @@ process.stdin.setRawMode(true);
 // called. The primary intent of writable.cork() is to accommodate a situation
 // in which several small chunks are written to the stream in rapid succession.
 // Instead of immediately forwarding them to the underlying destination,
-// writable.cork()buffers all the chunks until writable.uncork() is called,
+// writable.cork() buffers all the chunks until writable.uncork() is called,
 // which will pass them all to writable._writev(), if present. This prevents
 // a head-of-line blocking situation where data is being buffered while
 // waiting for the first small chunk to be processed. However, use of
-// writable.cork() without implementingwritable._writev() may have an adverse
+// writable.cork() without implementing writable._writev() may have an adverse
 // effect on throughput.
 
 process.stdin.on("data", (data) => {
