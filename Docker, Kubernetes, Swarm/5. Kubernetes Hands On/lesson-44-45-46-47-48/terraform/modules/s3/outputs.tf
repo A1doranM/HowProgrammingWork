e60014fe1acdd551bb9681c4291b2bfe26d227ed
A1doranM <vaselisk1997@gmail.com>
2024-03-04 10:59:30 +0200

    Kubernetes Hands On

--- Docker, Kubernetes, Swarm/5. Kubernetes Hands On/lesson-44-45-46-47-48/terraform/modules/s3/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "s3_bucket_arn" {
-  value       = aws_s3_bucket.bucket.arn
-  description = "AWS S3 Bucket ARN."
-}
-
-output "s3_bucket_id" {
-  value       = aws_s3_bucket.bucket.id
-  description = "AWS S3 Bucket name."
-}
