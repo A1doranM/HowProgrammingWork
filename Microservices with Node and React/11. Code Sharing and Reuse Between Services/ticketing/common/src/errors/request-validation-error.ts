ce875315bd285f2f32e83d9aa81df87b0f0aa699
A1doranM <vaselisk1997@gmail.com>
2025-05-11 13:47:34 +0300

    Microservices With NodeJS and React

---
+++ Microservices with Node and React/11. Code Sharing and Reuse Between Services/ticketing/common/src/errors/request-validation-error.ts
@@ -0,0 +1,22 @@
+import { ValidationError } from 'express-validator';
+import { CustomError } from './custom-error';
+
+export class RequestValidationError extends CustomError {
+  statusCode = 400;
+
+  constructor(public errors: ValidationError[]) {
+    super('Invalid request parameters');
+
+    // Only because we are extending a built in class
+    Object.setPrototypeOf(this, RequestValidationError.prototype);
+  }
+
+  serializeErrors() {
+    return this.errors.map((err) => {
+      if (err.type === 'field') {
+        return { message: err.msg, field: err.path };
+      }
+      return { message: err.msg };
+    });
+  }
+}
